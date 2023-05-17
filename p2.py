c45aa40ef010a12afffcd41ff4fcfe6663fd8462
Sergey Belov <bsv78rus@mail.ru>
2023-05-17 21:59:50 +0300

    обновление pr 22 #2

--- p2.py
+++ p2.py
@@ -2,3 +2,4 @@ print('для отказа от коммита')
 # for first commin new_way
 # for fourth comment
 # коммит из проекта pr22
+# коммит 2 из проекта pr22
--- p3.py
+++ p3.py
@@ -1,3 +1,5 @@
 # for third commit
 # for first commit new_way
 # коммит из проекта pr22
+###
+# коммит 2 из проекта pr22
